4c364dc70aeb1eb93b9a41972c67353ca1587991
Tulino, Ryan <rtulino@radial.com>
2017-05-24 11:14:29 -0400

    no logging here

--- app/services/slack/send_message.rb
+++ app/services/slack/send_message.rb
@@ -2,8 +2,7 @@ module Slack
   class SendMessage
     def self.call(channel, title, message)
       request = GenerateSendMessageRequest.new(channel).call(title, message)
-      response = SlackApiClient.new.send_message(request)
-      Rails.logger.debug response.body
+      SlackApiClient.new.send_message(request)
     end
   end
 end
